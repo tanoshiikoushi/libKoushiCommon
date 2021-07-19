d4eca09b06204aaec5016e32aa417ec0feb32dcf
tanoshiikoushi <68407204+tanoshiikoushi@users.noreply.github.com>
2021-07-18 21:07:39 -0600

    fixed some typos

--- include/rw_utilities/mem_read_utilities.h
+++ include/rw_utilities/mem_read_utilities.h
@@ -3,14 +3,14 @@
 #include "types/typedefs.h"
 
 // read unsigned values
-u8 read_8LE(char* data);
+u8 read_8LE(u8* data);
 constexpr auto read_8BE = read_8LE;
-u16 read_16LE(char* data);
-u16 read_16BE(char* data);
-u32 read_32LE(char* data);
-u32 read_32BE(char* data);
-u64 read_64LE(char* data);
-u64 read_64BE(char* data);
+u16 read_16LE(u8* data);
+u16 read_16BE(u8* data);
+u32 read_32LE(u8* data);
+u32 read_32BE(u8* data);
+u64 read_64LE(u8* data);
+u64 read_64BE(u8* data);
 
 #define MEM_READ_UTILITIES_H_INCLUDED
 
