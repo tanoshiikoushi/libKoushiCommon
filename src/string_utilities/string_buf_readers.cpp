c79d72d6c9584e2a5a97a11afa0d9c01192a10d6
tanoshiikoushi <68407204+tanoshiikoushi@users.noreply.github.com>
2021-07-21 22:43:45 -0600

    can you tell I'm getting tired?

--- src/string_utilities/string_buf_readers.cpp
+++ src/string_utilities/string_buf_readers.cpp
@@ -2,8 +2,8 @@
 #include "string_utilities/string_buf_readers.h"
 
 u8* read_dynamic_string(const u8* source) {
-    u64 str_size = strlen(source);
-    u8* ret = new char[str_size + 1];
-    strncpy((char*)ret, source, str_size);
+    u64 str_size = strlen((char*)source);
+    u8* ret = new u8[str_size + 1];
+    strncpy((char*)ret, (char*)source, str_size);
     return ret;
 }
